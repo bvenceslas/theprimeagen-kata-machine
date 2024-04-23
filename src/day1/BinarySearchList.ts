4d861f87561bda7d3cfb59b4ecce8574d864372d
bvenceslas <bvenceslas@gmail.com>
2024-04-23 21:26:51 +0200

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        // divide the array to get the middle
+        const middle = Math.floor(low + (high - low) / 2);
+        const value = haystack[middle];
+        //tests
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            // means we have to look to the lower values: left
+            high = middle;
+        } else {
+            // means we have to look to the higher values: right
+            low = middle + 1;
+        }
+    } while (low < high);
+    return false;
 }
