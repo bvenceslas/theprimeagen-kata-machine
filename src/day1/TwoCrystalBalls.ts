8d847fbd2f73e3b555789e27ba6c7ff80aefe2f2
bvenceslas <bvenceslas@gmail.com>
2024-05-01 09:09:09 +0200

    solved two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -2,4 +2,24 @@
 // distance, determine the exact spot in which it will break in the most
 // optimized way.
 
-export default function two_crystal_balls(breaks: boolean[]): number {}
+export default function two_crystal_balls(breaks: boolean[]): number {
+    // specify the jumping distance (sqrt(N))
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jmpAmount;
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // we go back sqrt(N)
+    i -= jmpAmount;
+    for (let j = 0; j < jmpAmount && j < breaks.length; j++, i++) {
+        // we linear by sqrt(N)
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
