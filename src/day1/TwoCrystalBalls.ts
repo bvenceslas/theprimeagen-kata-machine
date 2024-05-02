03eb1dc6456ff8a550733b6a3cd6b0636ea5e35e
bvenceslas <bvenceslas@gmail.com>
2024-05-02 09:09:09 +0200

    start two crystal

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,5 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+//Given two crystal balls that will break if dropped from high enough
+// distance, determine the exact spot in which it will break in the most
+// optimized way.
 
-}
+export default function two_crystal_balls(breaks: boolean[]): number {}
